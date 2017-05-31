4d89e7388b00b7fccff0d1871997d4f5c02de3b2
KMB <kibecker@stud.mediadesign.de>
2017-05-31 17:46:18 +0200

    Repro

---
+++ Abgabe_Thomas_Hummes_KB_1/Abgabe_Wahrscheinlichkeit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extantions
+{
+    public static class  Abgabe_Wahrscheinlichkeit
+    {
+        public static T First<T>(this IList<T> x) // this List nur mit static 
+        {
+
+            return x[0];
+        }
+
+        // Generic constraint
+        public static bool FirstEqual<T>(this IList<T> x, T value) where T : IComparable
+        {
+
+            return x[0].CompareTo(value) == 0;
+        }
+
+        public static void Bubblesort<T>(this IList<T> x) where T : IComparable
+        {
+            for (int i = 0; i < x.Count; i++)
+            {
+                for (int j = 0; j < x.Count - 1; j++)
+                {
+                    if (x[j].CompareTo(x[j + 1]) >= 1) // x[j]CompareTo(x)
+                    {
+                        T temp = x[j];
+                        x[j] = x[j + 1];
+                        x[j + 1] = temp;
+                    }
+
+                }
+            }
+
+        }
+    }
+
+}
+
---
+++ Abgabe_Thomas_Hummes_KB_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abgabe_Thomas_Hummes_KB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> enemy = new Dictionary<string, double>() ;
+            enemy.Add("Enemy1", 16);
+            enemy.Add("Enemy2", 3);
+            enemy.Add("Enemy3", 3);
+
+
+
+            List<int> enemyList = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine(string.Join(",", enemyList));
+        }
+    }
+}
